cde5f9cb1f2e7363a4c1e9db523ebfad63ab8692
Lucas <lucas14112003@gmail.com>
2023-11-07 13:38:50 -0300

    add mqtt client

--- src/app.js
+++ src/app.js
@@ -3,13 +3,18 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/');
+const mqtt = require('mqtt')
+const client = mqtt.connect('mqtt://localhost:1883');
 
+client.on('connect', ()=>{
+    console.log("broker on...");
+})
 //models
 const mqttModel = require('./models/mqtt');
 
 //rotas
 const appRoute = require('./routes/app-route');
-const mqtt = require('./models/mqtt');
+const mqttRoute = require('./models/mqtt');
 app.use('/', appRoute);
 
 //servidor
