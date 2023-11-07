9a23bfac6941ac0d5fb5ea570aa68338a21a95f0
Lucas <lucas14112003@gmail.com>
2023-11-07 13:41:00 -0300

    comentário

--- src/app.js
+++ src/app.js
@@ -6,9 +6,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/');
 const mqtt = require('mqtt')
 const client = mqtt.connect('mqtt://localhost:1883');
 
+//mqtt on
 client.on('connect', ()=>{
     console.log("broker on...");
 })
+
 //models
 const mqttModel = require('./models/mqtt');
 
